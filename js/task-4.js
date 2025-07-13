27bea55c56321ff432ce4e4125812ebfe9c1d6c4
Oleh Yatsenko <yatsenko.prog@gmail.com>
2025-07-13 16:34:06 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault(); // 🚫 Забороняємо перезавантаження
+
+  const { email, password } = form.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  form.reset(); // Очищаємо форму
+});
 
